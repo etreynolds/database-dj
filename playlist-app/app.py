2c01e1f8c96ad295aedbe578c21e51f7aebfa3d6
Ethan Reynolds <ethan.t.reynolds@gmail.com>
2023-02-18 16:49:21 -0700

    add new-playlist, new-song view functions

--- playlist-app/app.py
+++ playlist-app/app.py
@@ -17,7 +17,7 @@ app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
 # Having the Debug Toolbar show redirects explicitly is often useful;
 # however, if you want to turn it off, you can uncomment this line:
 #
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -47,6 +47,10 @@ def show_playlist(playlist_id):
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
+    playlist = Playlist.query.get(playlist_id)
+
+    return render_template("playlist.html", playlist=playlist)
+
 
 @app.route("/playlists/add", methods=["GET", "POST"])
 def add_playlist():
@@ -57,6 +61,23 @@ def add_playlist():
     """
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+
+    form = PlaylistForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+
+        playlist = Playlist(name=name, description=description)
+
+        db.session.add(playlist)
+        db.session.commit()
+
+        return redirect("/playlists")
+
+    else:
+
+        return render_template("new_playlist.html", form=form)
 
 
 ##############################################################################
@@ -77,6 +98,10 @@ def show_song(song_id):
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
+    song = Song.query.get(song_id)
+
+    return render_template("song.html", song=song)
+
 
 @app.route("/songs/add", methods=["GET", "POST"])
 def add_song():
@@ -87,6 +112,23 @@ def add_song():
     """
 
     # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+
+    form = SongForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        artist = form.artist.data
+
+        song = Song(title=title, artist=artist)
+
+        db.session.add(song)
+        db.session.commit()
+
+        return redirect("/songs")
+
+    else:
+
+        return render_template("new_song.html", form=form)
 
 
 @app.route("/playlists/<int:playlist_id>/add-song", methods=["GET", "POST"])
@@ -107,10 +149,10 @@ def add_song_to_playlist(playlist_id):
 
     if form.validate_on_submit():
 
-          # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+        # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
 
-          return redirect(f"/playlists/{playlist_id}")
+        return redirect(f"/playlists/{playlist_id}")
 
     return render_template("add_song_to_playlist.html",
-                             playlist=playlist,
-                             form=form)
+                           playlist=playlist,
+                           form=form)
--- playlist-app/forms.py
+++ playlist-app/forms.py
@@ -1,22 +1,28 @@
 """Forms for playlist app."""
 
-from wtforms import SelectField
+from wtforms import SelectField, StringField
+from wtforms.validators import InputRequired, Length
 from flask_wtf import FlaskForm
 
 
 class PlaylistForm(FlaskForm):
     """Form for adding playlists."""
 
-    # Add the necessary code to use this form
+    name = StringField("Name", validators=[
+                       InputRequired(), Length(min=1, max=25)])
+    description = StringField("Description", validators=[
+                              InputRequired(), Length(min=1, max=100)])
 
 
 class SongForm(FlaskForm):
     """Form for adding songs."""
 
-    # Add the necessary code to use this form
-
+    title = StringField("Title", validators=[InputRequired()])
+    artist = StringField("Artist", validators=[InputRequired()])
 
 # DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
+
+
 class NewSongForPlaylistForm(FlaskForm):
     """Form for adding a song to playlist."""
 
